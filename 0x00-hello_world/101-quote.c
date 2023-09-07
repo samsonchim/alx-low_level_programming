db8e4d4858bfe7b73bfc2daa9c0a1865ccc8d146
Samson Chi <fiercesamsonchi@gmail.com>
2023-09-07 15:17:58 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,25 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the specified message to the standard error.
+ *
+ * Return: 1
+ */
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = 59; /* Length of the message */
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = 59; /* Length of the message */
 
-    ssize_t bytes_written = write(2, message, len);
+	ssize_t bytes_written = write(2, message, len);
 
-    if (bytes_written == len)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+	if (bytes_written == len)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
