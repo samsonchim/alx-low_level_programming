fa6f66bbc1a07c064c45fa996a406bbb06c8df7e
Samson Chi <fiercesamsonchi@gmail.com>
2023-09-07 15:04:10 +0100

    Added some files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int main(void)
+{
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = 59; // Length of the message
+
+    ssize_t bytes_written = write(2, message, len);
+
+    if (bytes_written == len)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
