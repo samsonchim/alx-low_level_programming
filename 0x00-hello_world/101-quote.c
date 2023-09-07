a04a0c643d6d966512665b4dd7be7bd83bb5036e
Samson Chi <fiercesamsonchi@gmail.com>
2023-09-07 15:12:16 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = 59; // Length of the message
+    ssize_t len = 59; /* Length of the message */
 
     ssize_t bytes_written = write(2, message, len);
 
