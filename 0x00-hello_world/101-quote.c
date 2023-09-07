adda78c0011c2c3cb802c29b9e733ab960552ca8
Samson Chi <fiercesamsonchi@gmail.com>
2023-09-07 15:21:42 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,17 +9,18 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = 59; /* Length of the message */
+    char *message =
+        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = 59; /* Length of the message */
 
-	ssize_t bytes_written = write(2, message, len);
+    ssize_t bytes_written = write(2, message, len);
 
-	if (bytes_written == len)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+    if (bytes_written == len)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
 }
