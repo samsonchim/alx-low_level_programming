7fd862dd9955cf7ac79619ec63cad614c9a77062
Samson Chi <fiercesamsonchi@gmail.com>
2023-09-09 12:10:34 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,37 +7,38 @@
  */
 int main(void)
 {
-	int tens1, ones1, tens2, ones2;
-	int first = 1;
+    int tens1, ones1, tens2, ones2;
+    int first = 1;
 
-	for (tens1 = 0; tens1 <= 9; tens1++)
-	{
-		for (ones1 = 0; ones1 <= 9; ones1++)
-		{
-			for (tens2 = tens1; tens2 <= 9; tens2++)
-			{
-				int startOnes2 = (tens1 == tens2) ? ones1 + 1 : 0;
+    for (tens1 = 0; tens1 <= 9; tens1++)
+    {
+        for (ones1 = 0; ones1 <= 9; ones1++)
+        {
+            for (tens2 = tens1; tens2 <= 9; tens2++)
+            {
+                int startOnes2 = (tens1 == tens2) ? ones1 + 1 : 0;
 
-				for (ones2 = startOnes2; ones2 <= 9; ones2++)
-				{
-					if (!(tens1 == tens2 && ones1 == ones2))
-					{
-						if (!first)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-						putchar(tens1 + '0');
-						putchar(ones1 + '0');
-						putchar(' ');
-						putchar(tens2 + '0');
-						putchar(ones2 + '0');
-						first = 0;
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+                for (ones2 = startOnes2; ones2 <= 9; ones2++)
+                {
+                    if (!(tens1 == tens2 && ones1 == ones2))
+                    {
+                        if (!first)
+                        {
+                            putchar(',');
+                            putchar(' ');
+                        }
+                        putchar(tens1 + '0');
+                        putchar(ones1 + '0');
+                        putchar(' ');
+                        putchar(tens2 + '0');
+                        putchar(ones2 + '0');
+                        first = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    putchar('\n');
+    return (0);
 }
