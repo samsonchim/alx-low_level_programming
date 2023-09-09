3570985b884e469f42cf7e3bed45b4db2d1969bd
Samson Chi <fiercesamsonchi@gmail.com>
2023-09-09 12:07:55 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,28 @@
  */
 int main(void)
 {
-    int digit1, digit2, digit3;
+	int digit1, digit2, digit3;
 
-    for (digit1 = 0; digit1 <= 7; digit1++)
-    {
-        for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
-        {
-            for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
-            {
-                putchar(digit1 + '0');
-                putchar(digit2 + '0');
-                putchar(digit3 + '0');
+	for (digit1 = 0; digit1 <= 7; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
+			{
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+				putchar(digit3 + '0');
 
-                if (digit1 != 7 || digit2 != 8 || digit3 != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+				if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
