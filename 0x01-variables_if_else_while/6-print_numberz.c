37cfd90133a7f18830caf0ba39ae4f5ad1df8f9f
Samson Chi <fiercesamsonchi@gmail.com>
2023-09-09 12:02:29 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Entry point, prints all single-digit numbers of base 10 followed by a new line.
+ * main - Entry point
+ *
+ * Description:
+ * Prints all single-digit numbers of base 10 followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int digit;
+    int digit;
 
-	for (digit = 0; digit <= 9; digit++)
-	{
-		putchar(digit + '0');
-	}
+    for (digit = 0; digit <= 9; digit++)
+    {
+        putchar(digit + '0');
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
