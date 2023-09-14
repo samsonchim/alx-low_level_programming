4753ab3b76e0957687d5d7d004427137c2a6cc79
Samson Chi <fiercesamsonchi@gmail.com>
2023-09-14 14:48:39 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
- * followed by a new line
+ * print_most_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
  */
-void more_numbers(void)
+void print_most_numbers(void)
 {
-	int i, j;
+	int c;
 
-	for (i = 0; i < 10; i++)
+	for (c = 48; c < 58; c++)
 	{
-		for (j = 0; j < 15; j++)
+		if (c != 50)
 		{
-			if (j >= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			if (c != 52)
+			{
+				_putchar(c);
+			}
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
